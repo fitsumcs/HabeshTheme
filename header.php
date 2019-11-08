47299fb2a44b6c249036df197938bb49da2fe00d
3020 <fitsumcs@gmail.com>
2019-11-08 12:05:45 +0300

    Update on Header and Footer

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title><?php bloginfo( 'name' )?></title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <?php wp_head();?>
 </head>
 <body>
     
