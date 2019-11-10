62b19f3791fe29410c23b6a224fc7c46a226c3ce
3020 <fitsumcs@gmail.com>
2019-11-10 15:48:22 +0300

    Removing comments.php

--- single.php
+++ single.php
@@ -5,7 +5,7 @@ if (have_posts( )):
     while(have_posts( )): 
         the_post( );
         get_template_part( 'Template-Parts/content', 'post' );
-
+        comments_template();
     endwhile;
 endif;
 get_footer( );
